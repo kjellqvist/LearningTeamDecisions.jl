c298a0299c0f938e4b0a9db8a4852c7b025cf07f
Documenter.jl <documenter@juliadocs.github.io>
2022-07-28 07:52:59 +0000

    build based on 39b3268

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LearningTeamDecisions","category":"page"},{"location":"#LearningTeamDecisions","page":"Home","title":"LearningTeamDecisions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LearningTeamDecisions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LearningTeamDecisions]","category":"page"}]
+}
